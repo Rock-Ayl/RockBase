db40ef8e0d031aee55236dcbd272d9fa38d6b313
Rock-Ayl <AnYongLiang4869@163.com>
2022-03-09 22:53:26 +0800

    修改：测试类

--- src/test/java/org/rock/main/MainApplicationTests.java
+++ src/test/java/org/rock/main/MainApplicationTests.java
@@ -1,13 +1,20 @@
 package org.rock.main;
 
 import org.junit.jupiter.api.Test;
+import org.rock.main.serivce.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MainApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    FileService fileService;
+
+    @Test
+    void test() {
+        FileService.FileIndexSearchResult result = fileService.search();
+        System.out.println(123);
+    }
 
 }
