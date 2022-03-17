affc182729a03c797a9305662a2f07dd05bc0f01
Rock-Ayl <AnYongLiang4869@163.com>
2022-03-17 19:52:11 +0800

    修改：neo4j集成-3

--- src/test/java/org/rock/base/db/Neo4jTests.java
+++ src/test/java/org/rock/base/db/Neo4jTests.java
@@ -1,7 +1,7 @@
 package org.rock.base.db;
 
 import org.junit.jupiter.api.Test;
-import org.rock.base.db.neo4j.BaseNeo4jService;
+import org.rock.base.db.neo4j.BasNeo4TemplateMapper;
 import org.rock.base.pojo.base.BaseNode;
 import org.rock.base.pojo.node.PersonNode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +11,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Neo4jTests {
 
     @Autowired
-    private BaseNeo4jService baseNeo4jService;
+    private BasNeo4TemplateMapper basNeo4TemplateMapper;
 
     @Test
     void createNode() {
         PersonNode personNode = new PersonNode();
-        personNode.setName("李四");
+        personNode.setName("王二麻子");
         personNode.setSex("男");
         BaseNode.createBuild(personNode);
-        baseNeo4jService.save(personNode);
+        basNeo4TemplateMapper.save(personNode);
     }
 
 }
