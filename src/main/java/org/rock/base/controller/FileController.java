b627171d6a206e9b1622d9e8286a10cfcbcc1fb0
Rock-Ayl <AnYongLiang4869@163.com>
2022-04-23 23:33:09 +0800

    修改：es基底优化-rollPage扩增

--- src/main/java/org/rock/base/controller/FileController.java
+++ src/main/java/org/rock/base/controller/FileController.java
@@ -2,9 +2,15 @@ package org.rock.base.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AbstractAggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.rock.base.auth.LoginAuth;
 import org.rock.base.common.JsonResponse;
 import org.rock.base.constant.HttpConst;
+import org.rock.base.db.elasticsearch.BaseElasticSearchService;
+import org.rock.base.pojo.index.FileIndex;
 import org.rock.base.serivce.test.TestElasticSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -35,8 +41,14 @@ public class FileController {
     @ApiOperation(value = "文件搜索")
     @GetMapping(value = "/search", produces = HttpConst.RESPONSE_HEADERS_CONTENT_TYPE_APPLICATION_JSON)
     public String search() {
+        //布尔查询
+        BoolQueryBuilder query = QueryBuilders.boolQuery();
+        //聚合搜索
+        AbstractAggregationBuilder abstractAggregationBuilder = AggregationBuilders.terms("md5Count").field("md5.keyword");
+        //查询
+        BaseElasticSearchService.RollPageResult<FileIndex> rollPageResult = testElasticSearchService.rollPage(FileIndex.class, query, abstractAggregationBuilder, null, null);
         //返回
-        return JsonResponse.success().put("result", null).toString();
+        return JsonResponse.success().put("result", rollPageResult).toString();
     }
 
 }
