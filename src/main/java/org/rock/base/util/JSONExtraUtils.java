7cff099f708f090ef579ea6b8b4033151404e3c6
Rock-Ayl <AnYongLiang4869@163.com>
2022-11-30 11:10:45 +0800

    修改：优化工具包-4

--- src/main/java/org/rock/base/util/JSONExtraUtils.java
+++ src/main/java/org/rock/base/util/JSONExtraUtils.java
@@ -1,7 +1,11 @@
 package org.rock.base.util;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * JSON 扩展工具包
@@ -12,27 +16,55 @@ import com.alibaba.fastjson.JSONObject;
 public class JSONExtraUtils {
 
     /**
-     * 深克隆,也可以同结构对象互相转化
+     * 深克隆单个对象,也可以将一个对象转化为另一个对象(当然,结构得基本一致或继承关系)
      *
-     * @param object       源对象,比如父对象
+     * @param object       源对象,比如父对象,不能是数组等结构
      * @param toJavaObject 目标对象
      * @return
      */
     public static <T> T deepClone(Object object, Class<T> toJavaObject) {
+        //解析ob
+        Object ob = JSON.toJSON(object);
         //判空
-        if (object == null) {
+        if (ob == null) {
             //过
             return null;
         }
-        //先转化为json
-        JSONObject json = (JSONObject) JSON.toJSON(object);
-        //判空
-        if (json == null) {
+        //如果不是Json(集合、特殊情况不做考虑,用steam一个个来)
+        if (ob instanceof JSONObject == false) {
             //过
             return null;
         }
+        //强转一下
+        JSONObject json = (JSONObject) ob;
         //再转化为实体
         return json.toJavaObject(toJavaObject);
+    }
+
+    /**
+     * 深克隆为数组对象,也可以将一个对象数组转化为另一个对象数组(当然,结构得基本一致或继承关系)
+     *
+     * @param listOrArrObject 源对象,比如父对象,必须是数组、集合等结构
+     * @param toJavaObject    目标对象
+     * @return
+     */
+    public static <T> List<T> deepCloneList(Object listOrArrObject, Class<T> toJavaObject) {
+        //解析ob
+        Object ob = JSON.toJSON(listOrArrObject);
+        //判空
+        if (ob == null) {
+            //过
+            return new ArrayList<>();
+        }
+        //如果不是JsonArray(JSON的用上面的)
+        if (ob instanceof JSONArray == false) {
+            //过
+            return new ArrayList<>();
+        }
+        //强转一下
+        JSONArray JsonArray = (JSONArray) ob;
+        //再转化为实体
+        return JsonArray.toJavaList(toJavaObject);
     }
 
 }
