724ab43f071f77abd485af6f72f63119128e67b9
Rock-Ayl <AnYongLiang4869@163.com>
2022-11-30 14:45:18 +0800

    修改：扩展mongo-5

--- src/test/java/org/rock/base/db/MongoTests.java
+++ src/test/java/org/rock/base/db/MongoTests.java
@@ -24,32 +24,43 @@ class MongoTests {
     private MongoTemplate mongoTemplate;
 
     @Test
-    void test() {
+    void allTest() {
+
         //创建
         TestDoc create = new TestDoc();
         create.setNumber("编号");
         testMongoService.create(create);
+
         //查询
         TestDoc old = testMongoService.get(TestDoc.class, create.getId());
         TestDoc update = new TestDoc();
         update.setId(old.getId());
         update.setValue("测试123");
+
         //更新
         testMongoService.updateSkipNull(update, old.getVer());
+
         //查询
         old = testMongoService.get(TestDoc.class, create.getId());
         Update update2 = new Update();
         update2.set("123", 45);
+
         //覆盖更新
         testMongoService.update(TestDoc.class, old.getId(), update2, old.getVer());
+
         //查询
         old = testMongoService.get(TestDoc.class, create.getId());
+        System.out.println(123);
+
+        //分页查询
+        BaseMongoService.RollPageResult<TestDoc> find = testMongoService.rollPage(TestDoc.class, new ArrayList<>(), null, null);
         System.out.println(123);
 
         //初始化批量更新
         BulkOperations bulkOperations = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, TestDoc.class);
         bulkOperations.updateMulti(new Query(Criteria.where("_id").is(old.getId())), new Update().set("444", 5));
         bulkOperations.updateMulti(new Query(Criteria.where("_id").is(old.getId())), new Update().set("555", 6));
+
         //执行批量更新
         bulkOperations.execute();
         System.out.println(123);
