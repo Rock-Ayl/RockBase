53722965c5b1b1f7caa81204486f0887167f2053
Rock-Ayl <AnYongLiang4869@163.com>
2023-06-17 15:00:14 +0800

    修改：工具包扩展-3

--- src/main/java/org/rock/base/util/JSONExtraUtils.java
+++ src/main/java/org/rock/base/util/JSONExtraUtils.java
@@ -1,6 +1,7 @@
 package org.rock.base.util;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,6 +46,32 @@ public class JSONExtraUtils {
         }
         //先转为string再转为对应实体,如果转为json对象再转实体某些特殊情况会报错
         return JSON.parseArray(JSON.toJSONString(listOrArrObject), toJavaObject);
+    }
+
+    /**
+     * 给json某个key该名称,注意,如果新名称存在,会被覆盖
+     *
+     * @param json    json
+     * @param oldName 旧名称
+     * @param newName 新名称
+     */
+    public static void rename(JSONObject json, String oldName, String newName) {
+        //判空
+        if (json == null || oldName == null || newName == null) {
+            //过
+            return;
+        }
+        //如果不存在旧名称
+        if (json.containsKey(oldName) == false) {
+            //过
+            return;
+        }
+        //获取值
+        Object value = json.get(oldName);
+        //删除旧key
+        json.remove(oldName);
+        //覆盖新名称
+        json.put(newName, value);
     }
 
 }
