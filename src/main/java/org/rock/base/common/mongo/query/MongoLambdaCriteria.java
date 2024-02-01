0917f753e47f2f6445d4454efeab76ff326cdbc4
Rock-Ayl <AnYongLiang4869@163.com>
2024-02-01 11:24:55 +0800

    修改：Lambda表达式 扩展mongo-5

---
+++ src/main/java/org/rock/base/common/mongo/query/MongoLambdaCriteria.java
@@ -0,0 +1,79 @@
+package org.rock.base.common.mongo.query;
+
+import org.springframework.data.mongodb.core.query.Criteria;
+
+/**
+ * 使用 Lambda表达式 构建 {@link Criteria}
+ *
+ * @Author ayl
+ * @Date 2024-01-30
+ */
+public class MongoLambdaCriteria {
+
+    //被封装的
+    private Criteria criteria;
+
+    /**
+     * 重写初始化
+     */
+    public MongoLambdaCriteria() {
+        //默认初始化一个
+        this.criteria = new Criteria();
+    }
+
+    /**
+     * 实现 where
+     *
+     * @param key key
+     * @return
+     */
+    public MongoLambdaCriteria where(String key) {
+        //重新初始化一个
+        this.criteria = Criteria.where(key);
+        //返回
+        return this;
+    }
+
+    /**
+     * 实现 and
+     *
+     * @param key key
+     * @return
+     */
+    public MongoLambdaCriteria and(String key) {
+        //实现
+        this.criteria.and(key);
+        //返回
+        return this;
+    }
+
+    /**
+     * 实现 is
+     *
+     * @param value value
+     * @return
+     */
+    public MongoLambdaCriteria is(Object value) {
+        //实现
+        this.criteria.is(value);
+        //返回
+        return this;
+    }
+
+    /**
+     * 返回内部 {@link Criteria}
+     *
+     * @return
+     */
+    public Criteria toCriteria() {
+        //返回
+        return this.criteria;
+    }
+
+    @Override
+    public String toString() {
+        //直接使用
+        return this.criteria.toString();
+    }
+
+}
