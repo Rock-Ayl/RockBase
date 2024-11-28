17071c536a77d298f18447dc3ab0159d65c2dc8d
Rock-Ayl <AnYongLiang4869@163.com>
2024-11-28 09:53:31 +0800

    修改：优化细节-22

--- src/main/java/com/rock/base/job/schedule/async/ThreadPoolTaskConfig.java
+++ src/main/java/com/rock/base/job/schedule/async/ThreadPoolTaskConfig.java
@@ -39,7 +39,7 @@ public class ThreadPoolTaskConfig {
         //核心线程数(默认线程数)
         executor.setCorePoolSize(coreCount + 1);
         //最大线程数
-        executor.setMaxPoolSize(coreCount * 5 + 1);
+        executor.setMaxPoolSize(coreCount * 4 + 1);
         //缓冲队列大小
         executor.setQueueCapacity(coreCount * 10);
         //允许线程空闲时间(单位:秒)
