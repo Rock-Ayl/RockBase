dd640cb98821fb5c64af3ed0725f669120e46800
Rock-Ayl <AnYongLiang4869@163.com>
2025-03-19 09:07:15 +0800

    修改：定时任务优化

--- src/main/java/com/rock/base/job/event/listener/TestEventListener.java
+++ src/main/java/com/rock/base/job/event/listener/TestEventListener.java
@@ -1,7 +1,7 @@
 package com.rock.base.job.event.listener;
 
 import com.rock.base.job.event.event.TestEvent;
-import com.rock.base.job.schedule.async.ThreadPoolTaskConfig;
+import com.rock.base.job.schedule.async.AsyncJobThreadPoolTaskConfig;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 public class TestEventListener {
 
     //加异步注解为异步、不加为同步
-    @Async(ThreadPoolTaskConfig.SYNC_TASK_POOL_EXECUTOR)
+    @Async(AsyncJobThreadPoolTaskConfig.SYNC_TASK_POOL_EXECUTOR)
     //监听注解
     @EventListener
     public void handleTestEvent(TestEvent event) {
